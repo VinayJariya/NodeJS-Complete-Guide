3da2981e4eaf766a14869ed065d8eadc0360427d
Vinay Jariya <vinayjariya@gmail.com>
2019-09-17 01:37:31 +0530

    Added Template Engines

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const path = require('path')
 const express = require('express');
 const bodyParser = require('body-parser');
 
-
-
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -17,7 +18,10 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    res.status(404).render('404', {
+        pageTitle: 'Page Not Found',
+        path: ''
+    })
 })
 
 app.listen(3000);
